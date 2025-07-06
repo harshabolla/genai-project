ef2a474cc603a0a3a1b052fec0115be469d04eee
harshabolla <harshabolla@gmial.com>
2025-07-06 22:43:13 +0530

    Initial commit

--- f.py
+++ f.py
@@ -11,7 +11,46 @@ pip install pymupdf
 pip install pymupdf
  pip install "unstructured[docx]"
  
+#git --version
+# https://github.com/harshabolla/genai-project
+first time 
+git init
+git remote add origin <your-repo-URL>
+echo .venv/ > .gitignore
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git push -u origin main
 
- 
+after changes 
+git status
+git add .
+git commit -m "Meaningful message"
+git push
 
- pip install --upgrade pymupdf==4.0.0 unstructured==0.12.2 onnxruntime==1.16.0  unstructured-inference==0.7.11
+
+
+second pc
+git clone <your-repo-URL>
+cd <repo-folder>
+
+Edit your code in VS Code.
+
+When ready to save changes to GitHub:
+
+bash
+Copy
+Edit
+git status
+git add .
+git commit -m "My changes on second PC"
+git push
+
+git pull
+
+git clone <repo-url>
+cd <repo>
+python -m venv .venv
+.venv\Scripts\activate  (or source .venv/bin/activate)
+pip install -r requirements.txt
+ pip install --upgrade pymupdf==4.0.0 unstructured==0.12.2 onnxruntime==1.16.0  unstructured-inference==0.7.11+41+
